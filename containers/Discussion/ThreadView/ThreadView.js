1dd3025cd06b8e0144ce3d8b07055dda2e42e7db
Preetham7765 <preethamkowshik@gmail.com>
2018-11-21 01:19:21 -0500

    get topic data such as author, time and location

--- containers/Discussion/ThreadView/ThreadView.js
+++ containers/Discussion/ThreadView/ThreadView.js
@@ -1,30 +1,26 @@
 import React from 'react';
 import { Platform, View, StyleSheet, FlatList } from 'react-native';
 import KeyboardSpacer from 'react-native-keyboard-spacer';
-
+import { GiftedChat } from 'react-native-gifted-chat';
 
 import CommentHead from '../../../components/DiscussionScreen/CommentHead/CommentHead';
 import CommentView from '../../../components/DiscussionScreen/CommentView/CommentView';
-import PlusButton from '../../../components/DiscussionScreen/Plus/Plus';
-import CommentWindow from '../CommentWindow/CommentWindow';
-import { GiftedChat } from 'react-native-gifted-chat';
 import Aux from '../../../hoc/Auxi';
-
 import SERVER_URL from '../../../constants/Config';
+import ErrorScreen from '../../../components/ErrorScreen/ErrorScreen';
+import { Location } from 'expo';
 
-export default class ThreadView extends React.Component{
+export default class ThreadView extends React.Component {
 
-    state = {
-        messages: []
-    }
+    state = {}
 
 
-    onSendNewComment = (messages= []) => {
+    onSendNewComment = (message = []) => {
 
         // send the data to the server.
-        const authorId =  messages[0].user._id;
-        const comment = messages[0].text;
-        
+        const authorId = message[0].user._id;
+        const comment = message[0].text;
+
         const url = `${SERVER_URL}/api/comments`;
         const newComment = {
             authorId: authorId,
@@ -32,87 +28,99 @@ export default class ThreadView extends React.Component{
             comment: comment
         }
         console.log(newComment);
-        fetch(url,{
-            method:'POST',
+        fetch(url, {
+            method: 'POST',
             headers: {
                 "Content-Type": "application/json; charset=utf-8",
             },
             body: JSON.stringify(newComment)
         })
-        .then((response) => { 
-            console.log("Sent new comment Successfully",response);
-            this.setState(previousState => ({
-                messages: GiftedChat.append(previousState.messages, messages),
-            }));
-        })
-        .catch((error) => {
-            console.log("Error new comment send failed", error);
-        });
+            .then((response) => {
+                console.log("Sent new comment Successfully", response);
+                let newState = { ...this.state };
+                newState.topic.comments = GiftedChat.append(this.state.topic.comments, message);
+                this.setState({ newState });
+            })
+            .catch((error) => {
+                console.log("Error new comment send failed", error);
+            });
 
     }
 
     renderMessage(props) {
-        const { currentMessage: { text: currText, user : {name : authorname} } } = props;
+        const { currentMessage: { text: currText, user: { name: authorname } } } = props;
 
-        return <CommentView 
-                authorName = {authorname}
-                commentDesc = {currText}
-                commentCtr = {10}
-                />
+        return <CommentView
+            authorName={authorname}
+            commentDesc={currText}
+            commentCtr={10}
+        />
     }
-    
 
-    render(){
-        return(
-            <Aux>
-                <View>
-                    <CommentHead/>
-                </View>
-                <GiftedChat
-                    messages={this.state.messages}
-                    keyboardShouldPersistTaps = {'always'}
-                    onSend={messages => this.onSendNewComment(messages)}
-                    user={{
-                        _id: "5bda0840335d2283c0d5d0ef",
-                        name: "Chris"
-                    }}
-                    renderMessage={this.renderMessage}
-                />
-                {Platform.OS === 'android' ? <KeyboardSpacer /> : null }
-            </Aux>
-            // </View>
-        );
+
+    render() {
+        console.log("ThreadView", this.state);
+        if (this.state.topic === undefined)
+            return <ErrorScreen errorMessage="Loading" />
+        else
+            return (
+                <Aux>
+                    <View>
+                        <CommentHead
+                            title={this.state.topic.title}
+                            description={this.state.topic.description}
+                            author={this.state.topic.author}
+                            time={this.state.topic.time}
+                            location={this.state.topic.location}
+                        />
+                    </View>
+                    <GiftedChat
+                        messages={this.state.topic.comments}
+                        keyboardShouldPersistTaps={'always'}
+                        onSend={message => this.onSendNewComment(message)}
+                        user={{
+                            _id: "5bda0840335d2283c0d5d0ef",
+                            name: "Chris"
+                        }}
+                        renderMessage={this.renderMessage}
+                    />
+                    {Platform.OS === 'android' ? <KeyboardSpacer /> : null}
+                </Aux>
+                // </View>
+            );
     }
 
     componentDidMount() {
         // fetch results from server. props will have the id of the buzz/event
         const url = `${SERVER_URL}/api/comments/${this.props.navigation.getParam('topicId', null)}`;
         fetch(url)
-        .then( (response) => {
-            return response.json();
-        })
-        .then( (respJson) => {
+            .then((response) => {
+                return response.json();
+            })
+            .then((respJson) => {
+                const topicLocation = {
+                    latitude: respJson.topic.location[1], 
+                    longitude: respJson.topic.location[0]
+                }
+                console.log(topicLocation);
 
-            console.log(respJson);
-            this.setState({messages: [... respJson]});
+                Location.reverseGeocodeAsync(topicLocation)
+                    .then(res => {
+                        // res is an Array of geocoding object (see below)
+                        // console.log("geolocation position", res);
+                        const creationDate = new Date(respJson.topic.time);
+                        const hours = creationDate.getHours();
+                        const minutes = creationDate.getMinutes();
+                        const time = ("0"+hours).slice(-2) + ":" + ("0"+minutes).slice(-2);
+                        respJson.topic.location = res[0].street;
+                        respJson.topic.time = time;
+                        // ressJson.topic.time = creationDate;
+                        console.log("New topic", respJson);
+                        this.setState({ ...respJson });
+                    })
+                    .catch(err => console.log(err))
 
-
-        });
-
-        // this.setState({
-        //     messages: [
-        //       {
-        //         _id: 1,
-        //         text: 'Hello developer',
-        //         createdAt: new Date(),
-        //         user: {
-        //           _id: 2,
-        //           name: 'React Native',
-        //           avatar: 'https://placeimg.com/140/140/any',
-        //         },
-        //       },
-        //     ],
-        //   })
+            });
     }
 
 }
