bb74c7a1af8983ce600a833546c0ca74791b4b85
Vrishali Bhor <vbhor@iu.edu>
2018-12-02 16:07:43 -0500

    Logout changes

--- containers/UserScreen/UserScreen.js
+++ containers/UserScreen/UserScreen.js
@@ -1,191 +1,257 @@
-import React from 'react';
-import { Image, ScrollView, View, AsyncStorage } from 'react-native';
+import React from "react";
+import { Image, ScrollView, View, AsyncStorage } from "react-native";
 import {
-    Accordion,
-    Body,
-    Button,
-    Card,
-    CardItem,
-    Container,
-    Content,
-    Icon,
-    Left,
-    List,
-    ListItem,
-    Right,
-    Text,
-    Thumbnail
-} from 'native-base';
-import * as Progress from 'react-native-progress';
+  Accordion,
+  Body,
+  Button,
+  Card,
+  CardItem,
+  Container,
+  Content,
+  Icon,
+  Left,
+  List,
+  ListItem,
+  Right,
+  Text,
+  Thumbnail
+} from "native-base";
+import * as Progress from "react-native-progress";
 import { deleteUser, getUser } from "../../actions/userActions";
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { Entypo, MaterialCommunityIcons } from '@expo/vector-icons';
-import styles from './Styles';
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { Entypo, MaterialCommunityIcons } from "@expo/vector-icons";
+import styles from "./Styles";
 import FreeBuzz from "../FreeBuzz/FreeBuzz";
 import { SERVER_URL } from "../../constants/Config";
 import { calcLevel } from "../../services/Level";
-import { AppLoading, Font } from 'expo';
+import { AppLoading, Font } from "expo";
 
 class UserScreen extends React.Component {
-    constructor() {
-        super();
-        let that = this;
-        this.user = {
-            id: '',
-            checkins: 0,
-            buzzes: 6,
-            upvotes: 10
-        };
-        //this.getUserInfo();
-        this.state = {
-            fontLoaded: false,
-            level: 3,
-            dataArray: [
-                {
-                    title: "Show More", content:
-                        <List>
-                            <ListItem>
-                                <Thumbnail size={10} source={require('../../assets/images/buzz3.png')} />
-                                <Text>Buzzes</Text>
-                                <Text>{this.user.buzzes}</Text>
-                            </ListItem>
-                            <ListItem>
-                                <MaterialCommunityIcons name="marker-check" size={32} color="green" />
-                                <Text>Check-ins</Text>
-                                <Text>{this.user.checkins}</Text>
-                            </ListItem>
-                            <ListItem>
-                                <Entypo name="thumbs-up" size={32} color="yellow" />
-                                <Text>Up Votes</Text>
-                                <Text>{this.user.upvotes}</Text>
-                            </ListItem>
-                        </List>
-                },
-            ],
-            progress: 0.5
-        }
-    }
-
-    async componentWillMount() {
-        // This is used to overcome the Font error thrown with android
-        try {
-            await Font.loadAsync({
-                Roboto: require("native-base/Fonts/Roboto.ttf"),
-                Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf")
-            });
-            this.setState({ fontLoaded: true });
-        } catch (error) {
-            console.log('error loading icon fonts', error);
-        }
-    }
-
-    // Grab User data
-    async componentDidMount() {
-        // this.props.getUser(this.user.id);
-        if (this.user.id != '') {
-
-            try {
-                const userId = await AsyncStorage.getItem('userId');
-                this.user.id = userId;
-            }
-            catch (error) {
-                console.log("UserScreen: failed to get userId", error);
-            }
-        }
-    }
-
-
-    static navigationOptions = {
-        title: 'Profile',
+  constructor() {
+    super();
+    let that = this;
+    this.user = {
+      id: "",
+      checkins: 0,
+      buzzes: 6,
+      upvotes: 10
     };
-
-
-    render() {
-
-        if (!this.state.fontLoaded && this.props.user.reputationScore === undefined) {
-            return <AppLoading />;
+    //this.getUserInfo();
+    this.state = {
+      fontLoaded: false,
+      level: 3,
+      dataArray: [
+        {
+          title: "Show More",
+          content: (
+            <List>
+              <ListItem>
+                <Thumbnail
+                  size={10}
+                  source={require("../../assets/images/buzz3.png")}
+                />
+                <Text>Buzzes</Text>
+                <Text>{this.user.buzzes}</Text>
+              </ListItem>
+              <ListItem>
+                <MaterialCommunityIcons
+                  name="marker-check"
+                  size={32}
+                  color="green"
+                />
+                <Text>Check-ins</Text>
+                <Text>{this.user.checkins}</Text>
+              </ListItem>
+              <ListItem>
+                <Entypo name="thumbs-up" size={32} color="yellow" />
+                <Text>Up Votes</Text>
+                <Text>{this.user.upvotes}</Text>
+              </ListItem>
+            </List>
+          )
         }
-        const { user } = this.props.user;
-        console.log(user.firstName + ' - rep : ' + user.reputationScore);
-        console.log(calcLevel(user.reputationScore));
-        let result = [0, 0];//calcLevel(this.props.user.reputationScore);
-        const level = result[0];
-        const prog = result[1];
-        return (
+      ],
+      progress: 0.5
+    };
+  }
 
+  async componentWillMount() {
+    // This is used to overcome the Font error thrown with android
+    try {
+      await Font.loadAsync({
+        Roboto: require("native-base/Fonts/Roboto.ttf"),
+        Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf")
+      });
+      this.setState({ fontLoaded: true });
+    } catch (error) {
+      console.log("error loading icon fonts", error);
+    }
+  }
 
-            <ScrollView style={styles.container}>
-                <Container>
-                    <Content>
-                        <Card style={[styles.card, user.enterprise ? styles.enterprise : styles.normal]}>
-                            <CardItem>
-                                <Image source={{ uri: 'http://nbww.com/wp-content/uploads/2016/06/fot-static-promo-0608.jpg' }} style={{ height: 100, width: null, flex: 1 }} />
-                            </CardItem>
-                            <CardItem cardBody>
-                                <Left>
-                                    <Thumbnail source={{ uri: 'https://image.flaticon.com/icons/png/512/206/206879.png' }} />
-                                    <MaterialCommunityIcons name="marker-check" size={32} color="green" />
-                                    <Body>
-                                        <Text>{user.firstName} {user.lastName}</Text>
-                                        <Text note>Level {level}</Text>
-                                    </Body>
-                                </Left>
-                                <Body>
-                                    <View style={styles.container}>
-                                        <Progress.Bar progress={prog} width={100} />
-                                    </View>
-                                </Body>
-                            </CardItem>
-                            <CardItem>
-                                <Left>
-                                    <Button transparent>
-                                        <Icon active name="thumbs-up" />
-                                        <Text>12 Likes</Text>
-                                    </Button>
-                                </Left>
-                                <Body>
-                                    <Button transparent>
-                                        <Thumbnail source={require('../../assets/images/buzz3.png')} />
-                                        <Text>2 Buzzes</Text>
-                                    </Button>
-                                </Body>
-                                <Right>
-                                    <Text>11h ago</Text>
-                                </Right>
-                            </CardItem>
-                            <CardItem>
-                                <Content padder>
-                                    <Accordion dataArray={this.state.dataArray} expandedIcon="add">
-                                    </Accordion>
-                                </Content>
-                            </CardItem>
-                            <CardItem>
-                                <FreeBuzz progress={.60} />
-                            </CardItem>
-                        </Card>
+  // Grab User data
+  async componentDidMount() {
+    // this.props.getUser(this.user.id);
+    if (this.user.id != "") {
+      try {
+        const userId = await AsyncStorage.getItem("userId");
+        this.user.id = userId;
+      } catch (error) {
+        console.log("UserScreen: failed to get userId", error);
+      }
+    }
+  }
 
-                    </Content>
-                </Container>
-            </ScrollView>
+  static navigationOptions = {
+    title: "Profile"
+  };
+
+  render() {
+    if (
+      !this.state.fontLoaded &&
+      this.props.user.reputationScore === undefined
+    ) {
+      return <AppLoading />;
+    }
+    const { user } = this.props.user;
+    console.log(user.firstName + " - rep : " + user.reputationScore);
+    console.log(calcLevel(user.reputationScore));
+    let result = [0, 0]; //calcLevel(this.props.user.reputationScore);
+    const level = result[0];
+    const prog = result[1];
+    return (
+      <ScrollView style={styles.container}>
+        <Container>
+          <Content>
+            <Card
+              style={[
+                styles.card,
+                user.enterprise ? styles.enterprise : styles.normal
+              ]}
+            >
+              <CardItem button onPress={() => this.onLogout()}>
+                <Text>Logout</Text>
+              </CardItem>
+              <CardItem>
+                <Image
+                  source={{
+                    uri:
+                      "http://nbww.com/wp-content/uploads/2016/06/fot-static-promo-0608.jpg"
+                  }}
+                  style={{ height: 100, width: null, flex: 1 }}
+                />
+              </CardItem>
+              <CardItem cardBody>
+                <Left>
+                  <Thumbnail
+                    source={{
+                      uri:
+                        "https://image.flaticon.com/icons/png/512/206/206879.png"
+                    }}
+                  />
+                  <MaterialCommunityIcons
+                    name="marker-check"
+                    size={32}
+                    color="green"
+                  />
+                  <Body>
+                    <Text>
+                      {user.firstName} {user.lastName}
+                    </Text>
+                    <Text note>Level {level}</Text>
+                  </Body>
+                </Left>
+                <Body>
+                  <View style={styles.container}>
+                    <Progress.Bar progress={prog} width={100} />
+                  </View>
+                </Body>
+              </CardItem>
+
+              <CardItem>
+                <Left>
+                  <Button transparent>
+                    <Icon active name="thumbs-up" />
+                    <Text>12 Likes</Text>
+                  </Button>
+                </Left>
+                <Body>
+                  <Button transparent>
+                    <Thumbnail
+                      source={require("../../assets/images/buzz3.png")}
+                    />
+                    <Text>2 Buzzes</Text>
+                  </Button>
+                </Body>
+                <Right>
+                  <Text>11h ago</Text>
+                </Right>
+              </CardItem>
+              <CardItem>
+                <Content padder>
+                  <Accordion
+                    dataArray={this.state.dataArray}
+                    expandedIcon="add"
+                  />
+                </Content>
+              </CardItem>
+              <CardItem>
+                <FreeBuzz progress={0.6} />
+              </CardItem>
+            </Card>
+          </Content>
+        </Container>
+      </ScrollView>
+    );
+  }
+
+  static getLevel(reputationScore) {
+    return reputationScore > 0 ? Math.round(Math.log(reputationScore)) : 0;
+  }
+  onLogout() {
+    console.log("in logout");
+    fetch(`${SERVER_URL}/api/users/logout`, {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+      //body: JSON.stringify(user)
+    })
+      .then(response => console.log(response.json()))
+      .then(res => {
+        console.log("done");
+
+        console.log("done1");
+        // AsyncStorage.clear;
+        AsyncStorage.removeItem("token");
+        AsyncStorage.removeItem("userId");
+
+        this.props.navigation.navigate("Login");
+        console.log("done1");
+      })
+      .catch(function(error) {
+        //console.log(user);
+        console.log(
+          "There has been a problem with your fetch operation: " + error.message
         );
-    }
-
-    static getLevel(reputationScore) {
-        return reputationScore > 0 ? Math.round(Math.log(reputationScore)) : 0;
-    }
+        // ADD THIS THROW error
+        throw error;
+      });
+  }
 }
 
 UserScreen.propTypes = {
-    getUser: PropTypes.func.isRequired,
-    user: PropTypes.object.isRequired
+  getUser: PropTypes.func.isRequired,
+  user: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
-    user: state.user
+  user: state.user
 });
 
 export default connect(
-    mapStateToProps,
-    { getUser, deleteUser }
+  mapStateToProps,
+  { getUser, deleteUser }
 )(UserScreen);
